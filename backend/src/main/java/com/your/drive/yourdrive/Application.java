11626a44fc91d82997b26e2a854b7a5b52bc5214
sopeal <micsobczyk@gmail.com>
2019-04-07 00:24:34 +0200

    own index

--- backend/src/main/java/com/your/drive/yourdrive/Application.java
+++ backend/src/main/java/com/your/drive/yourdrive/Application.java
@@ -3,10 +3,29 @@ package com.your.drive.yourdrive;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.oauth2.client.EnableOAuth2Sso;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.security.Principal;
+
 
 @SpringBootApplication
 @EnableOAuth2Sso
-public class Application {
+@RestController
+public class Application extends WebSecurityConfigurerAdapter {
+
+	@RequestMapping("/user")
+	public Principal user(Principal principal) {
+		return principal;
+	}
+
+	@Override
+	protected void configure(HttpSecurity http) throws Exception {
+		http.antMatcher("/**").authorizeRequests().antMatchers("/", "/login**", "/webjars/**", "/error**").permitAll().anyRequest()
+				.authenticated();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
