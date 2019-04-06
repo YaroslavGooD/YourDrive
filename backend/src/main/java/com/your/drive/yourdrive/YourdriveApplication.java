c345a6990f6146ca0a37b98739e56d493d562b20
sopeal <micsobczyk@gmail.com>
2019-04-06 22:45:05 +0200

    initial

---
+++ backend/src/main/java/com/your/drive/yourdrive/YourdriveApplication.java
@@ -0,0 +1,13 @@
+package com.your.drive.yourdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YourdriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(YourdriveApplication.class, args);
+	}
+
+}
