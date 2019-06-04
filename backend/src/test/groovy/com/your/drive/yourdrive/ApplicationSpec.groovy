39ee8ce4d736864ba5c2497bfc819811323ed372
sopeal <micsobczyk@gmail.com>
2019-06-04 11:13:17 +0200

    added h2 integration

--- backend/src/test/groovy/com/your/drive/yourdrive/ApplicationSpec.groovy
+++ backend/src/test/groovy/com/your/drive/yourdrive/ApplicationSpec.groovy
@@ -1,12 +1,11 @@
 package com.your.drive.yourdrive
 
-import org.junit.experimental.categories.Category
+
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.context.ApplicationContext
 import spock.lang.Specification
 
-@Category(IntegrationTest.class)
 @SpringBootTest(
         classes = Application,
         webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT
