bab9bdc98538ffc9b3790dc91c3794ffde9c4bd8
Grala96 <210192@edu.p.lodz.pl>
2019-06-21 11:54:07 +0200

    Some changes in flyway migration

--- backend/src/main/java/com/your/drive/yourdrive/Application.java
+++ backend/src/main/java/com/your/drive/yourdrive/Application.java
@@ -1,5 +1,6 @@
 package com.your.drive.yourdrive;
 
+import com.zaxxer.hikari.HikariDataSource;
 import org.flywaydb.core.Flyway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -9,8 +10,16 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
 	public static void main(String[] args) {
+		final HikariDataSource ds = new HikariDataSource();
+		ds.setMaximumPoolSize(20);
+		ds.setDriverClassName("jdbc:mariadb://localhost:3306/your_drive");
+		ds.setJdbcUrl("jdbc:mariadb://localhost:3306/db");
+		ds.addDataSourceProperty("user", "app_user");
+		ds.addDataSourceProperty("password", "root");
+		ds.setAutoCommit(false);
+
 		// Create the Flyway instance and point it to the database
-		Flyway flyway = Flyway.configure().dataSource("jdbc:mysql://database:3306/your_drive", "app_user", "root").load();
+		Flyway flyway = Flyway.configure().dataSource("jdbc:mariadb://localhost:3306/your_drive", "app_user", "root").load();
 
 		// Start the migration
 		flyway.migrate();
